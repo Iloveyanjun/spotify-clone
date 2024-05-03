719bc367034f379fd3b8bde3f621ec66a23ce447
Homer <126698401+96buh@users.noreply.github.com>
2024-05-03 07:50:03 +0000

    add new layout to signup page and setup Supabase Auth

---
+++ app/(general)/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div className="px-2 py-2 bg-primary flex-grow overflow-hidden hover:overflow-y-auto">
+            <div>
+                
+            </div>
+            
+            
+            
+        </div>
+    );
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { login, signup } from "@/app/actions";
 
 export default function Header() {
     return (
@@ -16,12 +17,20 @@ export default function Header() {
             </div>
             {/* 登入和註冊按鈕 */}
             <div>
-                <button className="text-inactive py-2 mr-8 hover:text-white font-bold hover:scale-[1.05]">
-                    Sign up
-                </button>
-                <button className="rounded-full bg-white text-black px-8 py-3 font-bold hover:scale-[1.05]">
-                    登入
-                </button>
+                <form>
+                    <button
+                        formAction={signup}
+                        className="text-inactive py-2 mr-8 hover:text-white font-bold hover:scale-[1.05]"
+                    >
+                        Sign up
+                    </button>
+                    <button
+                        formAction={login}
+                        className="rounded-full bg-white text-black px-8 py-3 font-bold hover:scale-[1.05]"
+                    >
+                        登入
+                    </button>
+                </form>
             </div>
         </header>
     );
