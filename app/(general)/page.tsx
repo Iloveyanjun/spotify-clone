ea5d9e02d0d4d7570f3beff75157f5d8d035ea21
Homer <j29823678@gmail.com>
2024-05-08 14:19:25 +0800

    setup Supabase custom smtp, add error page

--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function HomePage() {
     return (
         <div className="px-2 py-2 bg-primary flex-grow overflow-hidden hover:overflow-y-auto">
             <div></div>
