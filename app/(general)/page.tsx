54aa5a2242e52588edac5d71a816a5e31256b8d7
Homer <126698401+96buh@users.noreply.github.com>
2024-05-10 17:31:22 +0000

    add get global weekly top songs, albums, artists

--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -1,8 +1,15 @@
-export default async function HomePage() {
+import Card from "@/components/Card";
+import { getWeeklyData } from "../actions";
 
+export default async function HomePage() {
     return (
         <div className="px-2 py-2 bg-primary flex-grow overflow-hidden hover:overflow-y-auto">
-            <div>123</div>
+            <form action={getWeeklyData}>
+                <button className="bg-blue-500 rounded-md hover:bg-red-400">
+                    scrap weekly popular data
+                </button>
+            </form>
+            <Card />
         </div>
     );
 }
--- app/actions.ts
+++ app/actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
+import puppeteer from "puppeteer";
 
 export async function login(user: unknown) {
     const supabase = createClient();
@@ -44,4 +45,56 @@ export async function logout() {
     const { error } = await supabase.auth.signOut();
 
     return redirect("/");
+}
+// 爬取每周受歡迎的十個歌曲, 專輯, 藝術家 
+export async function getWeeklyData() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(process.env.SPOTIFY_CHART_URL!);
+
+    // 等待一些時間以確保頁面完全加載
+    await page.waitForSelector("#__next");
+    // 找到按鈕
+    const buttons = await page.$$(".Button-sc-1dqy6lx-0");
+
+    const buttonTypes: { [key: string]: string } = {
+        3: "songs",
+        4: "albums",
+        5: "artists",
+    };
+    const albumData: { [key: string]: string[] } = {};
+
+    for (const index in buttonTypes) {
+        const buttonIndex = parseInt(index);
+        const type = buttonTypes[index];
+        // chart.spotify.com
+        // index 3:songs, 4:albums, 5:artists
+        await buttons[buttonIndex].click();
+
+        // 使用evaluate函數從網頁中提取專輯名稱
+        const albumNames = await page.evaluate(() => {
+            const albumElements = document.querySelectorAll(
+                "#__next > div > div > main > div > div > section > div > ol > div > li > div > p"
+            );
+            const albums = Array.from(albumElements).map((album) =>
+                album.textContent?.trim()
+            );
+            return albums.filter((album) => album !== undefined) as string[];
+        });
+
+        albumData[type] = albumNames;
+    }
+
+    await browser.close();
+    // 處理數據寫入database
+    const supabase = createClient();
+    const { songs, albums, artists } = albumData;
+
+    for (let i = 0; i < songs.length; i++) {
+        const { error } = await supabase.from("weeklyTopData").insert({
+            songs: `${songs[i]}`,
+            albums: `${albums[i]}`,
+            artists: `${artists[i]}`,
+        });
+    }
 }
