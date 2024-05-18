7ef579f926b7bee319a3e0d807f671c548fb6445
Homer <126698401+96buh@users.noreply.github.com>
2024-05-18 16:52:11 +0800

    Update page.tsx

--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -4,7 +4,7 @@ export default async function HomePage() {
     return (
         <div className="px-2 py-2 bg-primary flex-grow overflow-hidden hover:overflow-y-auto">
             <PopularArtists />
-            {/* <PopularAlbums /> */}
+            <PopularAlbums /> 
         </div>
     );
 }
