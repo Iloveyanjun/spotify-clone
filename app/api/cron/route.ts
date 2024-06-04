4df80ebb38ee385babadbe3145cafa4c5e0796ab
Homer <j29823678@gmail.com>
2024-06-04 15:17:21 +0800

    cron8

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -8,9 +8,7 @@ export async function GET(req: Request) {
     const browser = await puppeteer.launch({
         args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
         defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(
-            `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
-        ),
+        executablePath: await chromium.executablePath,
         headless: chromium.headless,
         ignoreHTTPSErrors: true,
     });
