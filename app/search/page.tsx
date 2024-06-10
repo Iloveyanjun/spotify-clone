faa86854c2914ca1ac85a89652f6ff065eafc2a7
normal <131578695+Iloveyanjun@users.noreply.github.com>
2024-06-10 12:02:48 +0800

    Update page.tsx

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,8 +1,42 @@
+import { search } from "@/apis/spotify";
+// I get the albums、artists、tracks
 
 export default async function SearchPage() {
-    
+  return (
+    <div className="px-2 py-2 bg-primary flex-grow overflow-hidden hover:overflow-y-auto">
+      <form action={search}>
+        <input
+          type="text"
+          className="bg-black-2000 text-right text-black"
+          name="query"
+          placeholder="search for songs"
+        />
+        <button style={{ marginLeft: "10px" }}>Press to search</button>
+      </form>
+      <div>
+        <h2 className="py-5 bg-primary flex-grow overflow-hidden hover:overflow-y-auto">
+          Albums:
+        </h2>
+        <div>
 
-    return (
-        <div className="px-2 py-2 bg-primary flex-grow overflow-hidden hover:overflow-y-auto"></div>
-    );
+
+        </div>
+        <h2 className="py-5 bg-primary flex-grow overflow-hidden hover:overflow-y-auto">
+          Artists:
+        </h2>
+        <div>
+
+
+        </div>
+        <h2 className="py-5 bg-primary flex-grow overflow-hidden hover:overflow-y-auto">
+          Tracks:
+        </h2>
+        <div>
+
+
+        </div>
+      </div>
+    </div>
+  );
 }
+
